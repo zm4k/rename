ab4d756fbc01bda512caef2442a308519d877b6b
aka <aka@user.noreply.github.com>
2017-10-24 23:40:38 +0200

    fish script added for reference

--- rename.pl
+++ rename.pl
@@ -39,4 +39,26 @@ print $name, "\n";
 #||  or
 ##!   not
 
+#########
+
+##!/usr/bin/fish
+##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+## Convert filenames to lowercase and replace characters recursively
+##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+##
+#if [ -z $argv ]
+  #echo Give target directory
+  #exit 0
+#end
+##
+#find $argv -depth -name '*' | while read file; 
+  #set -g  directory (dirname $file)
+  #set -g oldfilename (basename $file)
+  #set -g newfilename (echo $oldfilename | tr [:blank:] '-' | tr -d '[](),;!{}<>=?/&%@$§+*|' | tr '_' '-' | sed 's/ß/ss/g' | sed 's/ä/a/g' | sed 's/ö/o/g' | sed 's/ü/u/g' | sed 's/Ä/a/g' | sed 's/Ö/o/g' | sed 's/Ü/u/g' | tr [:upper:] [:lower:] | sed 's/_-_/-/g' | sed 's/----/-/g' | sed 's/---/-/g' | sed 's/--/-/g' ) 
+    #if [ $oldfilename != $newfilename ]
+      #mv -i $directory/$oldfilename $directory/$newfilename
+      #echo "$directory/$oldfilename ---> $directory/$newfilename"
+    #end
+#end
+
 
